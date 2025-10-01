e9201cd6eefc0613d6a4f3f77ad241ee03b74884
MY-2357 <wsmt.zmy@gmail.com>
2025-10-01 15:22:35 +0800

    finished 2.1.2

---
+++ lesson2/build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/zmy/Desktop/ros2_learn/lesson2
+# Build directory: /home/zmy/Desktop/ros2_learn/lesson2/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
