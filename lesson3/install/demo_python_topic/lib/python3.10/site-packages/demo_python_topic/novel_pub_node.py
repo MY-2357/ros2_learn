d03abacff41794d0a0cd6e2db13fbeb0f597303c
MY-2357 <wsmt.zmy@gmail.com>
2025-10-03 12:50:28 +0800

    finished 3.2.2

--- lesson3/install/demo_python_topic/lib/python3.10/site-packages/demo_python_topic/novel_pub_node.py
+++ lesson3/install/demo_python_topic/lib/python3.10/site-packages/demo_python_topic/novel_pub_node.py
@@ -10,7 +10,7 @@ class NovelPubNode(Node):
         super().__init__(node_name)
         self.get_logger().info(f"{node_name}，启动！")
         self.novels_queue_ = Queue()
-        self.novel_publisher_ = self.create_publisher(String, "novel", 10)
+        self.novel_publisher_ = self.create_publisher(String, 'novel', 10)
 
     def timer_callback(self):
         if self.novels_queue_.qsize() > 0:
@@ -26,11 +26,11 @@ class NovelPubNode(Node):
         self.get_logger().info(f"下载{url} ,{len(text)}")
         for line in text.splitlines():
             self.novels_queue_.put(line)
-        self.create_timer(2, self.timer_callback)
+        self.create_timer(1, self.timer_callback)
 
 def main():
     rclpy.init()
     node = NovelPubNode("novel_publish_node")
-    node.download("http://127.0.0.1:8000/novel1.txt")
+    node.download("http://127.0.0.1:8000/novel2.txt")
     rclpy.spin(node)
     rclpy.shutdown()
